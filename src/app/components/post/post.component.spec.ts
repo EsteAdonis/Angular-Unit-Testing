f3c5110df3ab6a7b0ffe6018bc9b8994e59cc997
EsteAdonis <julio.e.ochoa.p@gmail.com>
2023-09-27 16:33:43 -0600

    fixture.nativeElement to test html elements

--- src/app/components/post/post.component.spec.ts
+++ src/app/components/post/post.component.spec.ts
@@ -2,6 +2,7 @@ import { Post } from "src/app/models/Post";
 import { PostComponent } from "./post.component";
 import { first } from "rxjs";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { NO_ERRORS_SCHEMA } from "@angular/core";
 
 
 describe('Post Component', () => { 
@@ -11,15 +12,17 @@ describe('Post Component', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [PostComponent]
+      declarations: [ PostComponent ],
+      schemas: [ NO_ERRORS_SCHEMA ]
     });
 
     fixture = TestBed.createComponent(PostComponent);
     comp = fixture.componentInstance;    
+    comp.post = post;
   })
 
   it('Should create PostComponent using TestBed', () => {
-    expect(comp).toBeDefined;
+    expect(comp).toBeDefined();
   });
 
   it('Should raise and event when the delete post is clicked', () => { 
@@ -30,4 +33,11 @@ describe('Post Component', () => {
 
     comp.onDeletePost(new MouseEvent('click'));
   })
+
+  it('Should render the post title in the anchor element', () => {
+    fixture.detectChanges();
+    const postElement: HTMLElement = fixture.nativeElement;
+    const a = postElement.querySelector('a');
+    expect(a?.textContent).toContain(post.title);
+  })  
 });
